ef9c030a5a9814977d9944c472e456b8876f4e31
Aditya Sao <adityasao105@gmail.com>
2024-03-23 16:06:51 -0400

    added map

--- social-justice-hackathon/src/App.js
+++ social-justice-hackathon/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-
+import React,{useState,useEffect,useRef} from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 function App() {
+
+  const mapContainer=useRef(null);
+  const[viewState,setViewState]=useState({
+    center:[-100.43,35],
+    zoom:5,
+    pitch:90
+  })
+  useEffect(()=>{
+    const map=new maplibregl.Map({
+      container:mapContainer.current,
+      style:'https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
+      ...viewState,
+    })
+
+    return ()=>{
+      map.remove();
+    };
+  },[]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div ref={mapContainer} style={{width:'100%',height:'90%',marginTop:'20px',position:'absolute'}}>
+
+      </div>
+    </>
   );
 }
 
