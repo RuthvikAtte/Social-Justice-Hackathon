5caaa199e39cf6fd1e2b9973ce3c448dc9bec8c2
Aditya Sao <adityasao105@gmail.com>
2024-03-23 15:32:36 -0400

    removed leaflet

--- social-justice-hackathon/src/App.js
+++ social-justice-hackathon/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import HeatMap from './components/Map.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <HeatMap />
-      </header>
+      
     </div>
   );
 }
