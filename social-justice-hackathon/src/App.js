f1cfaea68cd341a90720ab802b6ff53b717931d5
Aditya Sao <adityasao105@gmail.com>
2024-03-24 02:41:26 -0400

    Tried adding heatmap

--- social-justice-hackathon/src/App.js
+++ social-justice-hackathon/src/App.js
@@ -1,19 +1,47 @@
 // import Map from './components/Map';
+"use client"
 import React,{useState} from 'react';
-import GoogleMapReact from 'google-map-react';
+import {GoogleMap,HeatmapLayer,useJsApiLoader,Marker} from '@react-google-maps/api'
+import airData from './Ait_Pollution_Data.json'
+
 function App() {
-  const position ={lat:53.54,lng:10}
+  const [map,setMap]=useState(null);
+
+const {isLoaded}=useJsApiLoader({
+  googleMapsApiKey:'AIzaSyBWzGNxAgTkCsGIjNj5iWB8f1JR8PMsPF8',
+  libraries:['visualization']
+})
+
+if(!isLoaded){
+  return <div>Loading...</div>
+}
+  const center ={lat:40.7128,lng:-74.0060}
   return (
-    <APIProvider apiKey=''>
-      <div style={{height:"100vh"}}>
-        <Map zoom={9} center={position} mapId={"424cd75cb51ec1bc"}></Map>
-      </div>
-    </APIProvider>
+    <div style={{height:'100vh'}}>
+    <GoogleMap mapContainerStyle={{position:'relative',width:'100%',height:'100%'}}
+    center={center}
+    zoom={12}
+    onLoad={(map)=>setMap(map)}
+    >
+      {map && airData &&
+      <>
+      <HeatmapLayer
+      data={airData.map((data)=>(
+        new window.google.maps.LatLng(data.latitude,data.longitude)
+        ))}
+        options={{
+          radius:'500'
+        }}
+        />
+        <Marker position={center}/>
+        </>
+        }
+
+
+
+    </GoogleMap>
+    </div>
   );
 }
 
 export default App;
-
-
-
-AIzaSyBWzGNxAgTkCsGIjNj5iWB8f1JR8PMsPF8
