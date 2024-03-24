7a4c122a4ce8957b6bb44085c857d456a7f823d8
Aditya Sao <adityasao105@gmail.com>
2024-03-24 01:37:22 -0400

    added google maps, need to get it to work

--- social-justice-hackathon/src/App.js
+++ social-justice-hackathon/src/App.js
@@ -1,11 +1,14 @@
-import Map from './components/Map';
-
+// import Map from './components/Map';
+import React,{useState} from 'react';
+import {APIProvider, Map,AdvancedMarker,Pin,InfoWindow,} from "@vis.gl/react-google-maps";
 function App() {
+  const position ={lat:53.54,lng:10}
   return (
-    <>
-      <Map/>
-      
-    </>
+    <APIProvider apiKey='AIzaSyBWzGNxAgTkCsGIjNj5iWB8f1JR8PMsPF8'>
+      <div style={{height:"100vh"}}>
+        <Map zoom={9} center={position} mapId={"424cd75cb51ec1bc"}></Map>
+      </div>
+    </APIProvider>
   );
 }
 
