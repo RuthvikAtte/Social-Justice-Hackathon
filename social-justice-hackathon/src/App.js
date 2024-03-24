3b32097168a76dd9ccafccb2e97e266b7bc804fe
Aditya Sao <adityasao105@gmail.com>
2024-03-24 09:20:17 -0400

    more finishing touches

--- social-justice-hackathon/src/App.js
+++ social-justice-hackathon/src/App.js
@@ -5,11 +5,9 @@ import airData from './Ait_Pollution_Data.json'
 import Map from "./components/Map"
 function App() {
 
-
 if(!isLoaded){
   return <div>Loading...</div>
 }
-  const center ={lat:40.7128,lng:-74.0060}
   return (
     <Map/>
   );
