cb15cae2fd36ee14f4aa0106981ef163c4daa841
Anish Jha <anishj2900@gmail.com>
2024-03-24 02:32:12 -0400

    redid ui for homepage

--- social-justice-hackathon/src/components/Home.js
+++ social-justice-hackathon/src/components/Home.js
@@ -12,10 +12,12 @@ export default function Home() {
     }, []);
     return (
         <div className="bg">
-            <h1>U.S. Heat Map</h1>
+            <div className="navbar">
+                <h1>U.S. Heat Map</h1>
+            </div>
             <div className="flex-parent">
                 <div className="flex-child filter" id="myBtnContainer">
-                    <button class="btn" onclick=""> Show all</button>
+                    <button class="btn" onclick="">Show all</button>
                     <button class="btn active" onclick="">Poverty</button>
                     <button class="btn" onclick="">Air Pollution</button>
                     <button class="btn" onclick="">Crime Rate</button>
@@ -26,7 +28,6 @@ export default function Home() {
             </div>
             <div className="slidecontainer">
                 <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
-                <p>Value: <span id="demo"></span></p>
             </div>
         </div>
     )
